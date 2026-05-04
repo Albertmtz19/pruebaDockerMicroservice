33bcf016af552a18e48b57d1661b2e75db43e226
Jose Alberto Martinez Hernandez <albertmtzh03@gmail.com>
2026-05-04 17:44:34 -0600

    feat(): se agregan querys personalizadas

--- src/main/java/com/prueba/agenda/repository/ContactoRepository.java
+++ src/main/java/com/prueba/agenda/repository/ContactoRepository.java
@@ -1,7 +1,17 @@
 package com.prueba.agenda.repository;
 
 import com.prueba.agenda.dto.Contacto;
+import org.hibernate.annotations.NamedNativeQuery;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ContactoRepository extends JpaRepository<Contacto, Long> {
+
+    @Query("select u from CONTACTO u where u.email = ?1")
+    Contacto findByEmailAddress(String email);
+
+
+    @Query("UPDATE CONTACTO SET STATUS = FALSE WHERE email = ?1")
+    Contacto desactivar(String email);
+
 }
