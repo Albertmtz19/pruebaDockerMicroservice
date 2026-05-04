931f5ffcf1fcb0698234220d87a4cd41d62236f9
Jose Alberto Martinez Hernandez <albertmtzh03@gmail.com>
2026-05-04 15:10:23 -0600

    feat(controller): se agregar controlador contacto

---
+++ src/main/java/com/prueba/agenda/controller/ContactoController.java
@@ -0,0 +1,26 @@
+package com.prueba.agenda.controller;
+
+import com.prueba.agenda.dto.Contacto;
+import com.prueba.agenda.service.ContactoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/contacto")
+public class ContactoController {
+    @Autowired
+    private ContactoService contactoService;
+
+    @GetMapping
+    public List<Contacto> listar (){
+        return contactoService.listar();
+    }
+
+    @PostMapping
+    public Contacto crear (@RequestBody Contacto c){
+        return contactoService.crear(c);
+    }
+
+}
