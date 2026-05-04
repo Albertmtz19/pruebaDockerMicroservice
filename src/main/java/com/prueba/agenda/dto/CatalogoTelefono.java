c4e5c369e492fba887930793d1499e4dcbbae3a4
Jose Alberto Martinez Hernandez <albertmtzh03@gmail.com>
2026-05-04 10:51:42 -0600

    feat(): Se agregan dtos

---
+++ src/main/java/com/prueba/agenda/dto/CatalogoTelefono.java
@@ -0,0 +1,31 @@
+package com.prueba.agenda.dto;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table("catalogotelefono")
+public class CatalogoTelefono {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String tipo;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+}
